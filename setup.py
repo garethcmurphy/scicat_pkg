0327c9a611df7e9401f32ffa9bb868a91d561580
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-20 14:59:22 +0200

    move files

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='scicat',
-    version='0.4',
+    version='0.5',
     author="Gareth Murphy",
     author_email="garethcmurphy@gmail.com",
     description="a scicat search package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/garethcmurphy/scicat_pkg",
-    packages=setuptools.find_packages(),
-    package_dir={"": "src"},
+    packages=['scicat'],
     install_requires=[
         "requests",
     ],
