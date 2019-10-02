ec51582a46eb26bf3135864bc7399200eb1efbf7
Gareth Murphy <garethcmurphy@gmail.com>
2019-10-02 11:51:05 +0200

    update api address

--- scicat/search.py
+++ scicat/search.py
@@ -7,7 +7,7 @@ def search(text, max_number_results):
     limit = {'limit': max_number_results, 'order': "creationTime:desc"}
     fields_encode = urllib.parse.quote(json.dumps(fields))
     limit_encode = urllib.parse.quote(json.dumps(limit))
-    dataset_url = "https://scicatapi.esss.dk/api/v3/Datasets/anonymousquery?fields=" + \
+    dataset_url = "https://scicat.esss.se/api/v3/Datasets/anonymousquery?fields=" + \
                 fields_encode+"&limits="+limit_encode
     r=requests.get(dataset_url).json()
     print(len(r), "result found!")
