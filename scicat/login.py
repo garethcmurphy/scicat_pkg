2f28a84ffc4f57fd6b78bbfc6398ab5351a718d8
Gareth Murphy <garethcmurphy@gmail.com>
2019-10-02 17:32:32 +0200

    add login

--- scicat/login.py
+++ scicat/login.py
@@ -1,37 +1,51 @@
 #!/usr/bin/env python3
 import getpass
 import requests
+import keyring
+
 from api import Api
-import json
+
+
+class Login:
+    token = ""
+    username = ""
+    password = ""
+
+    def login(self):
+        credentials = {}
+        try:
+            username = getpass.getuser()
+            # username = username.replace(".", "")
+            print("Username", username)
+        except:
+            username = input("Username:")
+        credentials["username"] = username
+        password = keyring.get_password('scicat', username)
+        if not password:
+            print("No password found in keychain, please enter it now to store it.")
+            password = getpass.getpass()
+            keyring.set_password('scicat', username, password)
+        credentials["password"] = password
+        api = Api()
+        url = api.base + "auth/msad"
+        token = ""
+        response = requests.post(url, json=credentials)
+        result = response.json()
+        print(result)
+        token = ""
+        if isinstance(token, str):
+            print(token)
+        else:
+            token = result.get("access_token")
+
+        return token
+
 
 def login():
-    credentials = {}
-    credentials["username"] = input("Username:")
-    try: 
-        credentials["password"] =  getpass.getpass() 
-    except Exception as error: 
-        print('ERROR', error) 
-    else: 
-        print('Password entered:' ) 
-    
-    api = Api()
-    
-    url = api.base + "auth/msad"
+    """main login"""
+    login = Login()
+    login.login()
 
 
-    print(url)
-    token =""
-    response = requests.post(url, json=credentials)
-    result = response.json()
-    print(result)
-    token = ""
-    token = result["access_token"]
-
-    return token
-
-def main():
-    login()
-    
 if __name__ == "__main__":
-    main()
-
+    login()
