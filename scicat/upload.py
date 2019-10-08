3cf480917a3c4595c98b729c210efeec24b4f4c5
Gareth Murphy <garethcmurphy@gmail.com>
2019-10-08 21:40:52 +0200

    add to upload

--- scicat/upload.py
+++ scicat/upload.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 """upload to scicat"""
+from api import Api
 
+
+class Upload:
+    """upload"""
+    dataset = {}
+
+    def __init__(self):
+        api = Api()
+        self.api = api.api
+
+    def create_json(self):
+        """create json"""
+        self.dataset = {
+            "pid": "10.17199/xlfghz"
+        }
+
+    def upload_scicat(self):
+        """upload to scicat"""
 
 def upload(name):
     """upload to scicat"""
