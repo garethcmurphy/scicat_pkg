882b4f882988196c3dec3239e0d7c6e7d98d72c9
Gareth Murphy <garethcmurphy@gmail.com>
2019-10-09 10:43:43 +0200

    add build

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!//usr/bin/env bash
+
+rm -rf dist scicat.egg-info build
+
+
+
+python3 setup.py sdist bdist_wheel
+
+python3 -m twine upload  dist/*
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='scicat',
-    version='0.5',
+    version='0.6',
     author="Gareth Murphy",
     author_email="garethcmurphy@gmail.com",
     description="a scicat search package",
