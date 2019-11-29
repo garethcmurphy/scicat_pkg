61d1c735621693bea2f2881a28698b47616bcc5f
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-29 10:27:09 +0100

    make search optional

--- scicat/search.py
+++ scicat/search.py
@@ -9,12 +9,12 @@ import requests
 from scicat import api
 
 
-def search(text, max_number_results):
+def search(text, limit=10):
     """search scicat"""
     fields = {'text': text}
-    limit = {'limit': max_number_results, 'order': "creationTime:desc"}
+    limits = {'limit': limit, 'order': "creationTime:desc"}
     fields_encode = urllib.parse.quote(json.dumps(fields))
-    limit_encode = urllib.parse.quote(json.dumps(limit))
+    limit_encode = urllib.parse.quote(json.dumps(limits))
     apix = api.Api()
     dataset_url = os.path.join(apix.api, "Datasets/anonymousquery?fields=") + \
         fields_encode+"&limits="+limit_encode
