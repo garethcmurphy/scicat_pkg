97273c2c325aa235df40d5864abc13170a243828
Gareth Murphy <garethcmurphy@gmail.com>
2020-02-27 12:26:27 +0100

    add download placeholder

--- scicat/__init__.py
+++ scicat/__init__.py
@@ -3,5 +3,6 @@ from .search import search
 from .api import Api
 from .login import login, accesstoken, userinfo
 from .attach import attach
+from .download import download
 from .upload import upload, widget
 from .base64_im import Base64Im
---
+++ scicat/download.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def download():
+    """download"""
+    pass
+
+if __name__ == "__main__":
+    pass
